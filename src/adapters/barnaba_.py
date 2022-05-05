fa0a9f52085e693613ea57020b18d7d7b19f4596
kamil <kamil.niznik2000@gmail.com>
2022-05-05 16:21:56 +0200

    feat: implement other interactions for barnaba

--- src/adapters/barnaba_.py
+++ src/adapters/barnaba_.py
@@ -1,44 +1,31 @@
 #! /usr/bin/env python
 # IMPORTANT! this file cannot be named barnaba.py, because it imports from "barnaba", and Python complains about that
-from lib2to3.pytree import Base
-from traceback import StackSummary
 import orjson
 import barnaba
 import tempfile
 import sys
 
-from typing import List
+from typing import List, Optional, Tuple
 
-from adapters.model import AnalysisOutput, BasePair, BasePhosphate, BaseRibose, LeontisWesthof, \
+from adapters.model import AnalysisOutput, BasePair, LeontisWesthof, OtherInteraction, \
     Residue, ResidueAuth, Stacking, StackingTopology
 
 
-def residue_from_pair(resinfo: str) -> Residue:
+def residue_from_pair(resinfo: str, chain_ids: List[str]) -> Residue:
     resinfo = resinfo.split('_')
-    # TODO: insertion code and chain
-    return Residue(None, ResidueAuth('A', int(resinfo[1]), '?', resinfo[0]))
+    # TODO: insertion code
+    return Residue(None, ResidueAuth(chain_ids[int(resinfo[2])], int(resinfo[1]), '?', resinfo[0]))
 
 
-def convert_lw(interaction: str) -> LeontisWesthof:
+def convert_interaction(interaction: str) -> Optional[LeontisWesthof]:
+    # Unknown interaction
+    if 'x' in interaction.lower():
+        return None
+
     if interaction in ('WCc', 'GUc'):
         return LeontisWesthof['cWW']
 
     return LeontisWesthof[f'{interaction[2]}{interaction[:2]}']
-
-
-def parse_base_pairs(pairings: List[str], res: List[str]) -> List[BasePair]:
-    base_pairs = []
-
-    for p in range(len(pairings[0][0])):
-        res1 = res[pairings[0][0][p][0]]
-        res2 = res[pairings[0][0][p][1]]
-        interaction = pairings[0][1][p]
-        nt1 = residue_from_pair(res1)
-        nt2 = residue_from_pair(res2)
-        lw = convert_lw(interaction)
-        base_pairs.append(BasePair(nt1, nt2, lw, None))
-
-    return base_pairs
 
 
 def convert_stacking_topology(topology: str) -> StackingTopology:
@@ -46,40 +33,67 @@ def convert_stacking_topology(topology: str) -> StackingTopology:
     return StackingTopology[name]
 
 
-def parse_base_stackings(stackings: List[str], res: List[str]) -> List[Stacking]:
-    base_stackings = []
+def parse_pdb(file_content: str) -> List[str]:
+    chain_ids = []
 
-    for p in range(len(stackings[0][0])):
-        res1 = res[stackings[0][0][p][0]]
-        res2 = res[stackings[0][0][p][1]]
-        interaction = stackings[0][1][p]
-        nt1 = residue_from_pair(res1)
-        nt2 = residue_from_pair(res2)
+    # Column 21 - chainID
+    for line in file_content.splitlines():
+        if line.startswith('ATOM'):
+            if line[21] not in chain_ids:
+                chain_ids.append(line[21])
+
+    return chain_ids
+
+
+def parse_barnaba(barnaba_result: Tuple[List[str], List[str], List[str]],
+                  chain_ids: List[str]) -> Tuple[List[BasePair], List[Stacking], List[OtherInteraction]]:
+
+    stackings, pairings, res = barnaba_result
+
+    base_pairs = []
+    base_stackings = []
+    other_interactions = []
+
+    for p, pairing in enumerate(pairings[0][0]):
+        res1 = res[pairing[0]]
+        res2 = res[pairing[1]]
+        interaction = pairings[0][1][p]
+        nt1 = residue_from_pair(res1, chain_ids)
+        nt2 = residue_from_pair(res2, chain_ids)
+        lw = convert_interaction(interaction)
+        if lw is None:
+            other_interactions.append(OtherInteraction(nt1, nt2))
+        else:
+            base_pairs.append(BasePair(nt1, nt2, lw, None))
+
+    for s, stacking in enumerate(stackings[0][0]):
+        res1 = res[stacking[0]]
+        res2 = res[stacking[1]]
+        interaction = stackings[0][1][s]
+        nt1 = residue_from_pair(res1, chain_ids)
+        nt2 = residue_from_pair(res2, chain_ids)
         topology = convert_stacking_topology(interaction)
         base_stackings.append(Stacking(nt1, nt2, topology))
 
-    return base_stackings
+    return base_pairs, base_stackings, other_interactions
 
 
 def analyze(file_content: str) -> AnalysisOutput:
-    suffix = '.pdb'
-
     directory = tempfile.TemporaryDirectory()
-    file = tempfile.NamedTemporaryFile('w+', dir=directory.name, suffix=suffix)
-    filename = file.name
-    file.write(file_content)
-    file.seek(0)
 
-    stackings, pairings, res = barnaba.annotate(filename)
+    with tempfile.NamedTemporaryFile('w+', dir=directory.name, suffix='.pdb') as file:
+        file.write(file_content)
+        file.seek(0)
+        barnaba_result = barnaba.annotate(file.name)
 
-    base_pairs = parse_base_pairs(pairings, res)
-    base_stackings = parse_base_stackings(stackings, res)
+    chain_ids = parse_pdb(file_content)
+    parsed_output = parse_barnaba(barnaba_result, chain_ids)
 
-    file.close()
-    return AnalysisOutput(base_pairs, base_stackings, None, None, None)
+    base_pairs, base_stackings, other_interactions = parsed_output
+    return AnalysisOutput(base_pairs, base_stackings, [], [], other_interactions)
 
 
-def main():
+def main() -> None:
     structure = analyze(sys.stdin.read())
     print(orjson.dumps(structure).decode('utf-8'))
 
