1481df6d2293def49072fc3a7cbc53a4e2f60da2
kamil <kamil.niznik2000@gmail.com>
2022-07-17 18:55:51 +0200

    refactor: BaRNAba

--- src/adapters/barnaba_.py
+++ src/adapters/barnaba_.py
@@ -1,5 +1,6 @@
 #! /usr/bin/env python
 # IMPORTANT! this file cannot be named barnaba.py, because it imports from "barnaba", and Python complains about that
+
 import orjson
 import barnaba
 import tempfile
@@ -12,140 +13,140 @@ from adapters.model import AnalysisOutput, BasePair, LeontisWesthof, OtherIntera
     Residue, ResidueAuth, Stacking, StackingTopology
 
 
-def residue_from_pair(
-    resinfo: str,
-    mapped_values: Tuple[List[str], Dict[str, Dict[int, Tuple[int, str]]]],
-) -> Residue:
-    resinfo_array = resinfo.split('_')
-    chain_ids, mapped_numbers = mapped_values
+class BarnabaAdapter:
 
-    chain_id = chain_ids[int(resinfo_array[2])]
-    name = resinfo_array[0]
-    mapped_number = int(resinfo_array[1])
-    number, icode = mapped_numbers[chain_id][mapped_number]
-    icode = '?' if icode == '' else icode
+    # Positions of resiudes info in PDB files
+    CHAIN_INDEX = 21
+    NUMBER_INDEX = slice(22, 26)
+    ICODE_INDEX = 26
 
-    return Residue(None, ResidueAuth(chain_id, number, icode, name))
+    # Tokens used in PDB files
+    ATOM = 'ATOM'
+    HETATM = 'HETATM'
+    TER = 'TER'
 
+    # BaRNAba uses different symbols for topology
+    # so we neeed to map them
+    STACKING_TOPOLOGIES = {
+        '>>': 'upward',
+        '<<': 'downward',
+        '<>': 'outward',
+        '><': 'inward',
+    }
 
-def convert_interaction(interaction: str) -> Optional[LeontisWesthof]:
-    # Unknown interaction
-    if 'x' in interaction.lower():
-        return None
+    # Since insertion code is required in our model
+    # we need a character to represent no insertion code
+    NO_ICODE_CHAR = '?'
 
-    if interaction in ('WCc', 'GUc'):
-        return LeontisWesthof['cWW']
+    def __init__(self) -> None:
+        # In the case of BaRNAba BasePhosphateIneractions
+        # and BaseRiboseInteractions are always empty
+        self.analysis_output = AnalysisOutput([], [], [], [], [])
+        # BaRNAba replaces chain identifiers with numbers
+        # so we need to remember them before processing
+        self.chains: List[str] = []
+        # Since BaRNAba does not use insertion code
+        # we need to renumber sequence numbers in PDB
+        # and remember old values - number and icode
+        # usage: new_numbers[chain][new_number] = (old_number, icode)
+        self.mapped_residues_info: Dict[str, Dict[int, Tuple[int, str]]] = defaultdict(dict)
 
-    return LeontisWesthof[f'{interaction[2]}{interaction[:2]}']
+    def get_residue(self, residue_info: str) -> Residue:
+        residue_info_list = residue_info.split('_')
+        chain = self.chains[int(residue_info_list[2])]
+        name = residue_info_list[0]
+        new_number = int(residue_info_list[1])
+        number, icode = self.mapped_residues_info[chain][new_number]
+        icode = self.NO_ICODE_CHAR if icode == '' else icode
+        return Residue(None, ResidueAuth(chain, number, icode, name))
 
+    def get_leontis_westhof(self, interaction: str) -> Optional[LeontisWesthof]:
+        # Unknown interaction for BaRNAba
+        # so classify it as OtherIneraction
+        if 'x' in interaction.lower():
+            return None
 
-def convert_stacking_topology(topology: str) -> StackingTopology:
-    name = {'>>': 'upward', '<<': 'downward', '<>': 'outward', '><': 'inward'}[topology]
-    return StackingTopology[name]
+        # BaRNAba uses these aliases for cWW in our model
+        if interaction in ('WCc', 'GUc'):
+            return LeontisWesthof.cWW
 
+        return LeontisWesthof[f'{interaction[2]}{interaction[:2]}']
 
-def parse_pdb(file_content: str) -> List[str]:
-    chain_ids = []
+    def append_chains(self, file_content: str) -> None:
+        for line in file_content.splitlines():
+            if line.startswith(self.ATOM) or line.startswith(self.HETATM):
+                if line[self.CHAIN_INDEX] not in self.chains:
+                    self.chains.append(line[self.CHAIN_INDEX])
 
-    # Column 21 - chainID (Character)
-    for line in file_content.splitlines():
-        if line.startswith('ATOM') or line.startswith('HETATM'):
-            if line[21] not in chain_ids:
-                chain_ids.append(line[21])
+    def append_interactions(self, pairings: List[str], residues: List[str]) -> None:
+        for p, pairing in enumerate(pairings[0][0]):
+            residue_info_left, residue_info_right = residues[pairing[0]], residues[pairing[1]]
+            interaction = pairings[0][1][p]
+            nt1 = self.get_residue(residue_info_left)
+            nt2 = self.get_residue(residue_info_right)
+            lw = self.get_leontis_westhof(interaction)
+            if lw is None:
+                self.analysis_output.otherInteractions.append(OtherInteraction(nt1, nt2))
+            else:
+                self.analysis_output.basePairs.append(BasePair(nt1, nt2, lw, None))
 
-    return chain_ids
+    def append_stackings(self, stackings: List[str], residues: List[str]) -> None:
+        for s, stacking in enumerate(stackings[0][0]):
+            residue_info_left, residue_info_right = residues[stacking[0]], residues[stacking[1]]
+            interaction = stackings[0][1][s]
+            nt1 = self.get_residue(residue_info_left)
+            nt2 = self.get_residue(residue_info_right)
+            topology = StackingTopology[self.STACKING_TOPOLOGIES[interaction]]
+            self.analysis_output.stackings.append(Stacking(nt1, nt2, topology))
 
+    # This function renumbers pdb and removes icode from file
+    def renumber_pdb(self, file_content: str) -> str:
+        # Counter for each chain
+        # usage: new_numbers[chain] = new_number
+        new_numbers = defaultdict(int)
+        # For efficiency save content in list and use join()
+        renumbered_content_list = []
 
-def parse_barnaba(
-    barnaba_result: Tuple[List[str], List[str], List[str]],
-    mapped_values: Tuple[List[str], Dict[str, Dict[int, Tuple[int, str]]]],
-) -> Tuple[List[BasePair], List[Stacking], List[OtherInteraction]]:
+        for line in file_content.splitlines(True):
+            if line.startswith(self.ATOM) or line.startswith(self.HETATM):
+                old_number = int(line[self.NUMBER_INDEX].strip())
+                icode = line[self.ICODE_INDEX].strip()
+                chain = line[self.CHAIN_INDEX].strip()
+                if (old_number, icode) not in self.mapped_residues_info[chain].values():
+                    new_numbers[chain] += 1
+                    self.mapped_residues_info[chain][new_numbers[chain]] = (old_number, icode)
+                new_line = f'{line[:22]}{str(new_numbers[chain]).rjust(4)[:4]} {line[27:]}'
+            elif line.startswith(self.TER):
+                new_line = f'{line[:22]}{str(new_numbers[chain]).rjust(4)[:4]} {line[27:]}'
+            else:
+                new_line = line
 
-    stackings, pairings, res = barnaba_result
+            renumbered_content_list.append(new_line)
 
-    base_pairs = []
-    base_stackings = []
-    other_interactions = []
+        renumbered_content = ''.join(renumbered_content_list)
+        return renumbered_content
 
-    for p, pairing in enumerate(pairings[0][0]):
-        res1 = res[pairing[0]]
-        res2 = res[pairing[1]]
-        interaction = pairings[0][1][p]
-        nt1 = residue_from_pair(res1, mapped_values)
-        nt2 = residue_from_pair(res2, mapped_values)
-        lw = convert_interaction(interaction)
-        if lw is None:
-            other_interactions.append(OtherInteraction(nt1, nt2))
-        else:
-            base_pairs.append(BasePair(nt1, nt2, lw, None))
+    def run_barnaba(self, file_content: str) -> str:
+        directory = tempfile.TemporaryDirectory()
+        with tempfile.NamedTemporaryFile('w+', dir=directory.name, suffix='.pdb') as file:
+            file.write(file_content)
+            file.seek(0)
+            barnaba_result = barnaba.annotate(file.name)
+        return barnaba_result
 
-    for s, stacking in enumerate(stackings[0][0]):
-        res1 = res[stacking[0]]
-        res2 = res[stacking[1]]
-        interaction = stackings[0][1][s]
-        nt1 = residue_from_pair(res1, mapped_values)
-        nt2 = residue_from_pair(res2, mapped_values)
-        topology = convert_stacking_topology(interaction)
-        base_stackings.append(Stacking(nt1, nt2, topology))
+    def analyze(self, file_content: str) -> AnalysisOutput:
+        self.append_chains(file_content)
+        renumbered_pdb: str = self.renumber_pdb(file_content)
+        stackings, pairings, res = self.run_barnaba(renumbered_pdb)
 
-    return base_pairs, base_stackings, other_interactions
-
-
-def renumber_pdb(file_content: str) -> Tuple[str, Dict[str, Dict[int, Tuple[int, str]]]]:
-    new_numbers = defaultdict(int)
-    mapped_numbers = defaultdict(dict)
-
-    renum_arr = []
-
-    # Column 21 - chainID (Character)
-    # Column 22-25 - resSeq (Integer)
-    # Column 26 - iCode (Character)
-
-    for line in file_content.splitlines(True):
-        new_line = ''
-
-        if line.startswith('ATOM') or line.startswith('HETATM'):
-            old_number = int(line[22:26].strip())
-            icode = line[26].strip()
-            chain_id = line[21].strip()
-
-            if (old_number, icode) not in mapped_numbers[chain_id].values():
-                new_numbers[chain_id] += 1
-                mapped_numbers[chain_id][new_numbers[chain_id]] = (old_number, icode)
-
-            new_line = f'{line[:22]}{str(new_numbers[chain_id]).rjust(4)[:4]} {line[27:]}'
-        elif line.startswith('TER'):
-            new_line = f'{line[:22]}{str(new_numbers[chain_id]).rjust(4)[:4]} {line[27:]}'
-        else:
-            new_line = line
-
-        renum_arr.append(new_line)
-
-    renum_content = ''.join(renum_arr)
-    return renum_content, mapped_numbers
-
-
-def analyze(file_content: str) -> AnalysisOutput:
-
-    chain_ids = parse_pdb(file_content)
-    renumbered_pdb, mapped_numbers = renumber_pdb(file_content)
-    mapped_values = (chain_ids, mapped_numbers)
-
-    directory = tempfile.TemporaryDirectory()
-
-    with tempfile.NamedTemporaryFile('w+', dir=directory.name, suffix='.pdb') as file:
-        file.write(renumbered_pdb)
-        file.seek(0)
-        barnaba_result = barnaba.annotate(file.name)
-
-    parsed_output = parse_barnaba(barnaba_result, mapped_values)
-
-    base_pairs, base_stackings, other_interactions = parsed_output
-    return AnalysisOutput(base_pairs, base_stackings, [], [], other_interactions)
+        self.append_interactions(pairings, res)
+        self.append_stackings(stackings, res)
+        return self.analysis_output
 
 
 def main() -> None:
-    structure = analyze(sys.stdin.read())
+    barnaba_adapter = BarnabaAdapter()
+    structure = barnaba_adapter.analyze(sys.stdin.read())
     print(orjson.dumps(structure).decode('utf-8'))
 
 
