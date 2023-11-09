e0da4a8578012491a89f381221cf47e37b834b86
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2023-11-09 09:55:30 +0100

    fix: handle bifurcated base pairs (#100)
    * feat: add test case
    
    * fix: correctly handle case of bifurcated base pairs
    
    * ref: name the test function better

--- tests/analysis/test_bpnet.py
+++ tests/analysis/test_bpnet.py
@@ -2,5 +2,11 @@ from adapters.analysis import bpnet
 
 
 def test_quintuple():
-    line = '      4       4   U ? 0       63    63   A ? 0    W:WT BP 0.72    39    39   G ? 0    h:hT TP 1.39    54    54   G ? 0    h:wT TP 1.47    41    41   A ? 0    W:HT BF 1.26'
+    line = "      4       4   U ? 0       63    63   A ? 0    W:WT BP 0.72    39    39   G ? 0    h:hT TP 1.39    54    54   G ? 0    h:wT TP 1.47    41    41   A ? 0    W:HT BF 1.26"
+    bpnet.parse_base_pairs(line)
+
+
+def test_bifurcated():
+    #            0     1   2 3 4       5  6    7     8     9   A B C       D  E    F
+    line = "    72    72   A ? 0    h:hT BF 1.37    76    76   C ? 0    h:wC BF 0.98"
     bpnet.parse_base_pairs(line)
