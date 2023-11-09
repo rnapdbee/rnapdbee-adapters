c921853f6cb9da7f97e38d5ed117470d774d6781
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2023-11-09 09:39:35 +0100

    ref: name the test function better

--- tests/analysis/test_bpnet.py
+++ tests/analysis/test_bpnet.py
@@ -6,7 +6,7 @@ def test_quintuple():
     bpnet.parse_base_pairs(line)
 
 
-def test_line():
+def test_bifurcated():
     #            0     1   2 3 4       5  6    7     8     9   A B C       D  E    F
     line = "    72    72   A ? 0    h:hT BF 1.37    76    76   C ? 0    h:wC BF 0.98"
     bpnet.parse_base_pairs(line)
