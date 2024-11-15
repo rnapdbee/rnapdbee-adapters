838c573f864959feafa86b438010e8b170b92ffa
Tomasz Żok <Tomasz.Zok@cs.put.poznan.pl>
2024-11-15 14:07:06 +0100

    fix: handle dashes in chain name for bpnet (#152)
    * feat: Add test for BPNET analysis of 200d-assembly1.cif structure
    
    * style: Apply linter fixes to test_bpnet.py
    
    * refactor: Remove unnecessary pytest import and error handling in BPNet test
    
    * fix: Update bpnet parsing to use new chain separator in Dockerfile and code
    
    * refactor: Update import and file path in test_bpnet.py
    
    * feat: add missing patch file
    
    * feat: add missing test file

---
+++ tests/analysis/test_bpnet.py
@@ -0,0 +1,9 @@
+from adapters.analysis.bpnet import analyze
+
+
+def test_analyze_200d_assembly1():
+    """Test if BPNET analysis of 200d-assembly1.cif raises appropriate exception"""
+    with open("files/input/200d-assembly1.cif") as f:
+        content = f.read()
+
+    analyze(content)
