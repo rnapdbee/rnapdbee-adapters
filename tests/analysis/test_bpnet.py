2257aa4f546cf31ba145ae5121a7d5805d954030
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2024-11-15 12:56:44 +0100

    refactor: Update import and file path in test_bpnet.py

--- tests/analysis/test_bpnet.py
+++ tests/analysis/test_bpnet.py
@@ -1,9 +1,9 @@
-from src.adapters.analysis.bpnet import analyze
+from adapters.analysis.bpnet import analyze
 
 
 def test_analyze_200d_assembly1():
     """Test if BPNET analysis of 200d-assembly1.cif raises appropriate exception"""
-    with open("tests/files/input/200d-assembly1.cif") as f:
+    with open("files/input/200d-assembly1.cif") as f:
         content = f.read()
 
     analyze(content)
