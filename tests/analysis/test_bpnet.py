c5265cc0da3269a4b115823666bfb3d62fe641b2
Tomasz Zok <tomasz.zok@cs.put.poznan.pl>
2024-11-15 12:08:12 +0100

    feat: Add test for BPNET analysis of 200d-assembly1.cif structure

---
+++ tests/analysis/test_bpnet.py
@@ -0,0 +1,12 @@
+import pytest
+from src.adapters.analysis.bpnet import analyze
+from src.adapters.exceptions import ThirdPartySoftwareError
+
+
+def test_analyze_200d_assembly1():
+    """Test if BPNET analysis of 200d-assembly1.cif raises appropriate exception"""
+    with open("tests/files/input/200d-assembly1.cif") as f:
+        content = f.read()
+    
+    with pytest.raises(ThirdPartySoftwareError):
+        analyze(content)
